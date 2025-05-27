5f77c46388dcc1c938c909ef7f352f55d2f84012
Leopold Jacquot <leopold.jacquot@infomaniak.com>
2025-05-27 16:28:10 +0200

    chore: init repo

---
+++ README.md
@@ -0,0 +1,83 @@
+# Calendar MCP Server
+
+MCP Server for the Calendar API.
+
+## Tools
+
+1. `calendar_list_events`
+    - Search in your calendar
+    - Returns: List of events
+
+## Setup
+
+1. Create a calendar token linked to your user:
+    - Visit the [API Token page](https://manager.infomaniak.com/v3/ng/accounts/token/list)
+    - Choose "workspace:calendar user_info" scopes
+
+### Usage with Claude Desktop
+
+Add the following to your `claude_desktop_config.json`:
+
+#### NPX
+
+```json
+{
+  "mcpServers": {
+    "calendar": {
+      "command": "npx",
+      "args": [
+        "-y",
+        "@infomaniak/mcp-server-calendar"
+      ],
+      "env": {
+        "CALENDAR_TOKEN": "your-token"
+      }
+    }
+  }
+}
+```
+
+#### docker
+
+```json
+{
+  "mcpServers": {
+    "calendar": {
+      "command": "docker",
+      "args": [
+        "run",
+        "-i",
+        "--rm",
+        "-e",
+        "CALENDAR_TOKEN",
+        "infomaniak/mcp-server-calendar"
+      ],
+      "env": {
+        "CALENDAR_TOKEN": "your-token"
+      }
+    }
+  }
+}
+```
+
+### Environment Variables
+
+1. `CALENDAR_TOKEN`: Required. Your calendar token.
+
+### Troubleshooting
+
+If you encounter permission errors, verify that:
+1. All required scopes are added to your calendar token
+2. The token is correctly copied to your configuration
+
+## Build
+
+Docker build:
+
+```bash
+docker build -t infomaniak/mcp-server-calendar -f Dockerfile .
+```
+
+## License
+
+This MCP server is licensed under the MIT License.
---
+++ src/index.ts
@@ -0,0 +1,184 @@
+#!/usr/bin/env node
+
+import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
+import {z} from "zod";
+import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio.js";
+
+const token = process.env.CALENDAR_TOKEN;
+
+if (!token) {
+    console.error(
+        "Please set CALENDAR_TOKEN environment variable",
+    );
+    process.exit(1);
+}
+
+const server = new McpServer(
+    {
+        name: "Infomaniak calendar MCP Server",
+        version: "0.0.1",
+    },
+    {
+        capabilities: {
+            completions: {},
+            prompts: {},
+            resources: {},
+            tools: {},
+        },
+    },
+);
+
+class CalendarClient {
+    private readonly headers: { Authorization: string; "Content-Type": string };
+
+    constructor() {
+        this.headers = {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        };
+    }
+
+    async listEvents(from: string, to: string): Promise<any> {
+        const params = new URLSearchParams({
+            from: from.replace("T", " "),
+            to: to.replace("T", " "),
+        });
+
+        const response = await fetch(
+            `https://api.infomaniak.com/1/calendar/pim/event?${params}`,
+            {headers: this.headers},
+        );
+
+        if (!response.ok) {
+            throw new Error('Something went wrong during event listing');
+        }
+
+        return response.json();
+    }
+
+    async getCalendars(): Promise<any> {
+        const response = await fetch(
+            `https://api.infomaniak.com/1/calendar/pim/calendar`,
+            {
+                headers: this.headers,
+            }
+        );
+
+        return response.json();
+    }
+
+    async getUserProfile(): Promise<any> {
+        const response = await fetch(
+            `https://api.infomaniak.com/2/profile`,
+            {
+                headers: this.headers,
+            }
+        );
+
+        return response.json();
+    }
+
+    async createEvent(title: string, start: string, end: string, description: string | undefined, attendees: string[] | undefined): Promise<any> {
+        const calendars = await this.getCalendars();
+        const profile = await this.getUserProfile();
+        let calendarAttendees: {
+            address: any;
+            className: string;
+            name: any;
+            organizer: boolean;
+            state: string;
+        }[] = [];
+
+        if (attendees) {
+            calendarAttendees = attendees.map((attendee: any) => ({
+                address: attendee,
+                className: "Attendee",
+                name: attendee,
+                organizer: false,
+                state: "NEEDS-ACTION",
+            }));
+
+            calendarAttendees.push({
+                address: profile.data.email,
+                className: "Attendee",
+                name: profile.data.display_name,
+                organizer: true,
+                state: "ACCEPTED",
+            })
+        }
+
+        const response = await fetch(
+            `https://api.infomaniak.com/1/calendar/pim/event`,
+            {
+                headers: this.headers,
+                method: "POST",
+                body: JSON.stringify({
+                    title,
+                    start: start.replace("T", " "),
+                    end: end.replace("T", " "),
+                    description,
+                    freebusy: "busy",
+                    type: "event",
+                    calendar_id: calendars.data.calendars[0].id,
+                    fullday: false,
+                    timezone_start: profile.data.preferences.timezone.name,
+                    timezone_end: profile.data.preferences.timezone.name,
+                    attendees: calendarAttendees
+                })
+            },
+        );
+
+        if (!response.ok) {
+            throw new Error(`Something went wrong during event creation ${await response.text()}`);
+        }
+
+        return response.json();
+    }
+}
+
+const calendarClient = new CalendarClient();
+
+server.tool(
+    "calendar_list_events",
+    "List Infomaniak calendar events within a specified time range",
+    {
+        from: z.string().describe("Start time (Date time string)"),
+        to: z.string().describe("End time (Date time string)")
+    },
+    async ({from, to}) => {
+        const response = await calendarClient.listEvents(from, to);
+
+        return {
+            content: [{type: "text", text: JSON.stringify(response.data)}],
+        };
+    }
+);
+
+server.tool(
+    "calendar_create_event",
+    "Create a new Infomaniak calendar event",
+    {
+        title: z.string().describe("Event title"),
+        start: z.string().describe("Event start time (Date time string)"),
+        end: z.string().describe("Event end time (Date time string)"),
+        description: z.string().describe("Event description").optional(),
+        attendees: z.array(z.string()).describe("List of attendee email addresses").optional(),
+    },
+    async ({title, start, end, description, attendees}) => {
+        const response = await calendarClient.createEvent(title, start, end, description, attendees);
+
+        return {
+            content: [{type: "text", text: JSON.stringify(response.data)}],
+        };
+    }
+);
+
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+}
+
+main().catch((error) => {
+    console.error("Fatal error in main():", error);
+    process.exit(1);
+});
